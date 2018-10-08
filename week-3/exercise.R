c61f9dc25ae48478d56e7beade09e0aafcf98848
kishore <viprankishore@gmail.com>
2018-10-08 15:48:01 -0700

    solutions to ex

--- week-3/exercise.R
+++ week-3/exercise.R
@@ -1,15 +1,25 @@
-iris_df <- data.frame(iris)
+install.packages('iris')
+library(iris)
 
 # Select all values of data frame for Sepal.length
+iris$Sepal.Length
+iris[, 'Sepal.Length']
 
 # Select only rows of Virginica flowers
+virginica <- iris[iris$Species == 'virginica', ]
 
-# Select only the sepal legnth of the Virginica flowers
+# Select only the sepal length of the Virginica flowers
+virginica$Sepal.Length
 
-# Select rows where Petal.Length > 4.0
+# Select rows of iris where Petal.Length > 4.0
+iris[iris$Petal.Length > 4, ]
 
 # Add a column to the dataframe indicating whether Petal.Length > 4.0
+iris$petal_length_greater <- iris$Petal.Length > 4.0
 
 # Find the species type of the flower that has the longest Sepal length
+iris[iris$Sepal.Length == max(iris$Sepal.Length), ]$Species
 
-# Select rows where species type is 'setosa' and Sepal.Width > 3.0
+# View rows where species type is 'setosa' and Sepal.Width > 3.0
+View(iris[((iris$Species == 'setosa') & (iris$Sepal.Width > 3.0)), ])
+
--- week-4/exercise.R
+++ week-4/exercise.R
@@ -1,59 +1,67 @@
 # Exercise 7: DPLYR practice with NBA data
-# install.packages("dplyr")
 library(dplyr)
 
 # Read in the NBA team data of the 2016-2017 season from the data directory  
 # into a variable called `team.data` using `read.csv`
-
+# Remember to not read strings in as factors.
+team.data <- read.csv("data/teams.csv", stringsAsFactors = FALSE)
 
 # The data.frame team.data should now be accessible to you. 
 # View it, and get some basic information about the number of rows/columns. 
 # Note the "X" preceding some of the column titles as well as the "*" 
 # following the names of teams that made it to the playoffs that year.
-
+View(team.data)
 
 
 # Add a column that gives the turnovers to steals ratio (TOV / STL) for each team
-
+team.data <- mutate(team.data, Ratio = TOV / STL)
 
 
 # Sort the teams from lowest turnover/steal ratio to highest
-
+team.data <- arrange(team.data, Ratio)
 
 # Get the team that had the highest Total Rebounds (TRB) only with the columns 
 # Team and TRB  *using one line of code*
-
+most.rebounds <- filter(team.data, TRB == max(TRB))%>% select(Team, TRB)
 
 # Print only the name of the team that had the highest total rebounds
 # (that also happens to be the greatest team of all time)
-
+print(most.rebounds$Team)
 
 
 ## Let's change gears!
 
 # Read in the Pokemon data from the data directory  
 # into a variable called `pokemon` using `read.csv`. Remember to not read strings in as factors.
+pokemon <- read.csv('data/Pokemon.csv', stringsAsFactors = FALSE)
 
 # First, View() the data set to see what info you have to work with 
-
+View(pokemon)
 
 # Find all the Pokemon that are "Water" Type 1 (or your favorite)
 # Save those rows into a variable called `water.pkm`
-
+water.pkm <- pokemon %>% filter(Type.1 == "Water")
 
 # Group the pokemon by Type 2 and save into a descriptive variable.
+grouped.water.pkm <- water.pkm %>% group_by(Type.2)
 
 # View your new variable -- did it affect anything?
 
-
+# # Nope! Why? Well, they're grouped, but there is nothing to aggregate or summarize
+# # rows by so they are still all displaying despite dplyr understanding it needs to group them.
 
 # Use summarize() to figure out what the mean (average) speed is for 
 # each different type in type 2 and what the maximum HP (health point) value is
 # within each group.
+grouped.water.pkm.by.generation <- grouped.water.pkm %>% summarize("Mean speed" = mean(Speed), "Max HP" = max(HP))
 
 # New pokemon are released in generations. Aggregate the data on generations
 # using summarize() to look at the minimum and maximum attack and defense values
 # across generations for Pokemon that are not "Legendary". 
 
 # Can you do it in one line?
+
+pokemon.specific <- pokemon %>% filter(Legendary == "False") %>% group_by(Generation) %>% summarize("Maximum attack" = max(Attack), "Minimum attack" = min(Attack), 
+                                                                                                    "Maximum defense" = max(Defense), "Minimum defense" = min(Defense))
+
 
